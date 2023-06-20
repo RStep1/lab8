00b875d96c917fa742f60f0868b4907713a787fe
RStep1 <stepanrykov.itmo@gmail.com>
2023-06-20 16:13:10 +0300

    Add database window items to controller

--- client/src/main/java/controllers/DatabaseWindowController.java
+++ client/src/main/java/controllers/DatabaseWindowController.java
@@ -4,10 +4,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import org.apache.commons.lang3.ObjectUtils.Null;
-
 import commands.QuitCommand;
 import data.CommandArguments;
+import data.TableRowVehicle;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,6 +14,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import run.MainLauncher;
@@ -29,7 +34,137 @@ public class DatabaseWindowController {
     private URL location;
 
     @FXML
+    private ChoiceBox<String> countByChoiceBox;
+
+    @FXML
+    private Label countLabel;
+
+    @FXML
     private Button databaseQuitButton;
+
+    @FXML
+    private TableColumn<TableRowVehicle, Long> distanceTravelledColumn;
+
+    @FXML
+    private TextField distanceTravelledField;
+
+    @FXML
+    private TableColumn<TableRowVehicle, Integer> enginePowerColumn;
+
+    @FXML
+    private TextField enginePowerField;
+
+    @FXML
+    private ChoiceBox<String> filterByChoiceBox;
+
+    @FXML
+    private TextField filteringValue;
+
+    @FXML
+    private ComboBox<String> fuelTypeChoice;
+
+    @FXML
+    private TableColumn<TableRowVehicle, String> fuelTypeColumn;
+
+    @FXML
+    private TableColumn<TableRowVehicle, Long> idColumn;
+
+    @FXML
+    private TextField idField;
+
+    @FXML
+    private Button infoButton;
+
+    @FXML
+    private Label initializationTimeLabel;
+
+    @FXML
+    private TableColumn<TableRowVehicle, Long> keyColumn;
+
+    @FXML
+    private TextField keyField;
+
+    @FXML
+    private Label lastModifiedTimeLabel;
+
+    @FXML
+    private TableColumn<TableRowVehicle, String> nameColumn;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private Button onClearButtonClick;
+
+    @FXML
+    private Button onCountButtonClick;
+
+    @FXML
+    private Button onFilterButtonClick;
+
+    @FXML
+    private Button onRemoveButtonClick;
+
+    @FXML
+    private TableColumn<TableRowVehicle, String> ownerColumn;
+
+    @FXML
+    private ChoiceBox<String> removeByChoiceBox;
+
+    @FXML
+    private Label tableRecords;
+
+    @FXML
+    private TextField valueToRemove;
+
+    @FXML
+    private TableView<TableRowVehicle> vehicleTable;
+
+    @FXML
+    private ComboBox<String> vehicleTypeChoice;
+
+    @FXML
+    private TableColumn<TableRowVehicle, String> vehicleTypeColumn;
+
+    @FXML
+    private Button visualizeButton;
+
+    @FXML
+    private TableColumn<TableRowVehicle, Float> xColumn;
+
+    @FXML
+    private TextField xField;
+
+    @FXML
+    private TableColumn<TableRowVehicle, Double> yColumn;
+
+    @FXML
+    private TextField yField;
+
+    @FXML
+    void onClearButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onInfoButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onInsertButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUpdateButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onVisualizeButtonClick(ActionEvent event) {
+
+    }
 
     @FXML
     void quit(ActionEvent event) {
@@ -61,7 +196,6 @@ public class DatabaseWindowController {
 
     @FXML
     void initialize() {
-
+        
     }
-
 }
