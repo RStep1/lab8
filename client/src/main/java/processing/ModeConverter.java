ad9114fddfa4130665cbab1ed3d0108243b484f8
RStep1 <stepanrykov.itmo@gmail.com>
2023-06-24 15:47:07 +0300

    Fix mode converter

--- client/src/main/java/processing/ModeConverter.java
+++ client/src/main/java/processing/ModeConverter.java
@@ -7,26 +7,26 @@ import data.FilterMode;
 import mods.RemoveMode;
 
 public class ModeConverter {
-    public static final Function<String, RemoveMode> GET_REMOVE_MODE = (newType) -> {
+    public static final Function<String, RemoveMode> GET_REMOVE_MODE = (newRemoveMode) -> {
         RemoveMode removeMode = null;
         for (RemoveMode mode : RemoveMode.values())
-            if (mode.getName().equals(removeMode.getName()))
+            if (mode.getName().equals(newRemoveMode))
                 removeMode = mode;
         return removeMode;
     };
 
-    public static final Function<String, FilterMode> GET_FILTER_MODE = (newType) -> {
+    public static final Function<String, FilterMode> GET_FILTER_MODE = (newFilterMode) -> {
         FilterMode filterMode = null;
         for (FilterMode mode : FilterMode.values())
-            if (mode.getName().equals(filterMode.getName()))
+            if (mode.getName().equals(newFilterMode))
                 filterMode = mode;
         return filterMode;
     };
 
-    public static final Function<String, CountMode> GET_COUNT_MODE = (newType) -> {
+    public static final Function<String, CountMode> GET_COUNT_MODE = (newCountMode) -> {
         CountMode countMode = null;
         for (CountMode mode : CountMode.values())
-            if (mode.getName().equals(countMode.getName()))
+            if (mode.getDisplayName().equals(newCountMode))
                 countMode = mode;
         return countMode;
     };
